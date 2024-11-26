1dbb2a5fdef0bd359eca0ed78aeebd646febccda
Terria-K <teuria@proton.me>
2024-11-26 21:39:36 +0800

    ECS Test

--- build.zig
+++ build.zig
@@ -82,17 +82,30 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
+    const main_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    main_tests.root_module.addImport("ziglangSet", ziglangSet.module("ziglangSet"));
+
+    const run_main_tests = b.addRunArtifact(main_tests);
+
+    const world_tests = b.addTest(.{
+        .root_source_file = b.path("src/engine/ecs/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    world_tests.root_module.addImport("ziglangSet", ziglangSet.module("ziglangSet"));
+
+    const run_world_tests = b.addRunArtifact(world_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_main_tests.step);
+    test_step.dependOn(&run_world_tests.step);
 }
--- src/engine/ecs/main.zig
+++ src/engine/ecs/main.zig
@@ -4,3 +4,71 @@ pub const Type = @import("typeid.zig").Type;
 pub const World = @import("World.zig");
 pub const filter = @import("filter.zig");
 pub const EntityID = u32;
+
+test "ecs" {
+    const EntityFilter = filter.EntityFilter;
+    const expect = std.testing.expect;
+    const ValueComponent = struct {
+        value: i32
+    };
+    const AdderComponent = struct {
+        adding: i32
+    };
+
+    const AddSystem = struct {
+        filter: *EntityFilter,
+        pub const filterWith = .{
+            ValueComponent,
+            AdderComponent
+        };
+
+        pub fn run(self: @This(), world: *World, _: anytype) void {
+            var iter = self.filter.entities.iterator();
+            while (iter.next()) |e| {
+                const adding = world.getReadOnlyComponent(AdderComponent, e.*);
+                var value = world.getComponent(ValueComponent, e.*);
+
+                value.value += adding.adding;
+            }
+        }
+    };
+
+    const SystemContainer = struct {
+        add_system: AddSystem
+    };
+    var world = try World.init(std.testing.allocator);
+    defer world.deinit();
+    var system_container = try world.createSystems(SystemContainer);
+    defer world.deinitSystems(system_container);
+
+    const entity1 = world.createEntity();
+    world.setComponent(ValueComponent, .{ .value = 1}, entity1);
+    world.setComponent(AdderComponent, .{ .adding = 1}, entity1);
+
+    const entity2 = world.createEntity();
+    world.setComponent(ValueComponent, .{ .value = 4 }, entity2);
+    world.setComponent(AdderComponent, .{ .adding = 3}, entity2);
+
+    const entity3 = world.createEntity();
+    world.setComponent(ValueComponent, .{ .value = 4 }, entity3);
+
+    world.runSystems(&system_container, .{});
+
+    const value1 = world.getReadOnlyComponent(ValueComponent, entity1).*;
+    const value2 = world.getReadOnlyComponent(ValueComponent, entity2).*;
+    const value3 = world.getReadOnlyComponent(ValueComponent, entity3).*;
+
+    try expect(value1.value == 2);
+    try expect(value2.value == 7);
+    try expect(value3.value == 4);
+
+    world.removeComponent(AdderComponent, entity2);
+
+    world.runSystems(&system_container, .{});
+
+    const value1New = world.getReadOnlyComponent(ValueComponent, entity1).*;
+    const value2New = world.getReadOnlyComponent(ValueComponent, entity2).*;
+
+    try expect(value1New.value == 3);
+    try expect(value2New.value == 7);
+}
